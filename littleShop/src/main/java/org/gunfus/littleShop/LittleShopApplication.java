e41d7d74c260cc9c5db4a18f46e6ed320007bbff
Angel Vera <gunfus@gmail.com>
2025-10-15 20:59:18 -0400

    Adding the project

---
+++ littleShop/src/main/java/org/gunfus/littleShop/LittleShopApplication.java
@@ -0,0 +1,13 @@
+package org.gunfus.littleShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleShopApplication.class, args);
+	}
+
+}
---
+++ littleShop/src/test/java/org/gunfus/littleShop/LittleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gunfus.littleShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
